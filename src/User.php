9f63e789728a79ff192c8cff8f7b2218867462ce
Stefan Watzinger <stefanwatzingerfrankfurt@gmail.com>
2024-08-27 12:51:58 +0200

    Mocks

--- src/User.php
+++ src/User.php
@@ -22,6 +22,10 @@ class User
      */
     public string $surname = "";
 
+    public string $email = "";
+
+    protected Mailer $mailer;
+
     /**
      * Get the user's full name from their first name and surname
      *
@@ -30,5 +34,15 @@ class User
     public function getFullName(): string
     {
         return trim("$this->first_name $this->surname");
+    }
+
+    public function notify($message): bool 
+    {
+        return $this->mailer->sendMessage($this->email, $message);
+    }
+
+    public function setMailer(Mailer $mailer): void 
+    {
+        $this->mailer = $mailer;
     }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,22 +4,51 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    
+    protected $user;
+
+    protected function setUp(): void 
+    {
+        $this->user = new User;
+    }
+
     public function testReturnsFullName()
     {
-        $user = new User;
-        $user->first_name = "Teresa";
-        $user->surname = "Green";
+        $this->user->first_name = "Teresa";
+        $this->user->surname = "Green";
         
-        $this->assertEquals('Teresa Green', $user->getFullName());
+        $this->assertEquals('Teresa Green', $this->user->getFullName());
         
     }
 
     public function testFullNameIsEmptyByDefault() 
     {
-        $user = new User;
-    
-        $this->assertEquals('', $user->getFullName());
+        $this->assertEquals('', $this->user->getFullName());
+    }
+
+    public function testNotificationIsSent() 
+    {
+        $mock_mailer = $this->createMock(Mailer::class);
+        $mock_mailer->expects($this->once())
+                    ->method('sendMessage')
+                    ->with($this->equalTo('test@test.de'), $this->equalTo('Hello'))
+                    ->willReturn(true);
+
+        $this->user->setMailer($mock_mailer);
+        $this->user->email = "test@test.de";
+
+        $this->assertTrue($this->user->notify("Hello"));
+    }
+
+    public function testCannotNotifyUserWithoutEmail(): void 
+    {
+        $mock_mailer = $this->getMockBuilder(Mailer::class)
+                            ->onlyMethods([])
+                            ->getMock();
+
+        $this->user->setMailer($mock_mailer);
+        $this->expectException(Exception::class);
+        $this->user->notify("Hello");
+        
     }
 
 }
